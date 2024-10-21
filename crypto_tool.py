5dff40aef3da531decc81bea6ea858599a192875
1viktoris1 <viktor.j.94@outlook.com>
2024-10-21 12:38:51 +0200

    made the code prettier.

--- crypto_tool.py
+++ crypto_tool.py
@@ -1,9 +1,9 @@
-# crypto_tool.py
-from cryptography.fernet import Fernet
+#Encryprytd or decrypts file using an given key
 import argparse
-import os
+from cryptography.fernet import Fernet
 
 def load_key(key_file):
+    """Loads a key from file"""
     try:
         with open(key_file, 'rb') as file:
             return file.read()
@@ -12,6 +12,7 @@ def load_key(key_file):
         exit(1)
 
 def encrypt_file(key, file_name):
+    """Encrypts a file using the provided key"""
     try:
         with open(file_name, 'rb') as file:
             data = file.read()
@@ -21,16 +22,18 @@ def encrypt_file(key, file_name):
 
         encrypted_file_name = file_name + '.encrypted'
         with open(encrypted_file_name, 'wb') as encrypted_file:
-            encrypted_file.write(encrypted_data)
-        
+            encrypted_file.write(encrypted_data) 
         print(f"File {file_name} is encrypted as {encrypted_file_name}")
 
     except FileNotFoundError:
         print(f"File {file_name} not found.")
+    except PermissionError:
+        print(f"No rights to file {file_name}.")
 
 def decrypt_file(key, file_name):
+    """Decrypts an encrypted file using the provided key"""
     if not file_name.endswith('.encrypted'):
-        print(f"File {file_name} is not encrypted (.encrypted förväntas).")
+        print(f"File {file_name} is not encrypted (.encrypted is expected).")
         return
 
     try:
@@ -43,22 +46,19 @@ def decrypt_file(key, file_name):
         original_file_name = file_name.replace('.encrypted', '')
         with open(original_file_name, 'wb') as decrypted_file:
             decrypted_file.write(decrypted_data)
-        
         print(f"File {file_name} is decrypted and saved as {original_file_name}")
 
     except FileNotFoundError:
         print(f"File {file_name} not found.")
-    except Exception as e:
-        print(f"failed to decrypt {file_name}. error: {e}")
+    except PermissionError:
+        print(f"No rights to open {file_name}.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="encrypt or decrypt file.")
     parser.add_argument("operation", choices=["encrypt", "decrypt"], help="Choose: 'encrypt' or 'decrypt'")
     parser.add_argument("key_file", help="Name for key to use for encrypt/decrypt.")
     parser.add_argument("file_name", help="Name of file to encrypt/decrypt.")
-    
     args = parser.parse_args()
-    
     key = load_key(args.key_file)
 
     if args.operation == "encrypt":
@@ -66,5 +66,6 @@ if __name__ == "__main__":
     elif args.operation == "decrypt":
         decrypt_file(key, args.file_name)
 
+# Example usage:
 #python crypto_tool.py encrypt <namn nyckel> <namn fil>
 #python crypto_tool.py decrypt <namn nyckel> <namn fil>
